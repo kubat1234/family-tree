3a895cc6cf6febf39a605ba929fe90ef19225fd2
Krzysiek <krzysiek6c25@gmail.com>
2025-05-09 12:46:24 +0200

    Database factory update

--- java/familytree/src/main/java/tcs/familytree/core/DatabaseFactoryPersonOnly.java
+++ java/familytree/src/main/java/tcs/familytree/core/DatabaseFactoryPersonOnly.java
@@ -6,6 +6,25 @@ import java.util.List;
 public class DatabaseFactoryPersonOnly implements DatabaseFactory {
     List<Person> list = new ArrayList<>();
     int counter = 0;
+    Class<DatabaseConection> databaseConnectionClass = null;
+
+    @Override
+    public DatabaseConection build() {
+        if(databaseConnectionClass == null){
+            return new DatabaseConnectionWithoutDatabase(this);
+        }
+        if(databaseConnectionClass.equals(DatabaseConnectionWithoutDatabase.class)){
+            return new DatabaseConnectionWithoutDatabase(this);
+        }
+        throw new NotImplemented("NOT IMPLEMENTED CLASS FACTORY:" + databaseConnectionClass);
+        //return null;
+    }
+
+    @Override
+    public DatabaseFactory changeType(Class<DatabaseConection> databaseConectionClass) {
+        this.databaseConnectionClass = databaseConectionClass;
+        return this;
+    }
 
     @Override
     public Person getPerson() {
