f9eda3869bed24db0882ef77d0651f615fcb839d
Jakub Sulkowski <jakub.sulkowski@student.uj.edu.pl>
2025-05-27 23:29:43 +0200

    CLI 49%

--- src/main/java/tcs/familytree/views/cli/CLIView.java
+++ src/main/java/tcs/familytree/views/cli/CLIView.java
@@ -1,11 +1,10 @@
 package tcs.familytree.views.cli;
 
 import tcs.familytree.core.person.Person;
-import tcs.familytree.services.FamilyGraph;
 import tcs.familytree.viewmodels.SingleTreeViewModel;
-import tcs.familytree.views.GraphView;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class CLIView {
     final SingleTreeViewModel viewModel;
@@ -19,34 +18,103 @@ public class CLIView {
         System.out.println(" -=-=- Program do drzew genealogicznych -=-=-");
         Mode currentMode = new BasicMode(viewModel);
         while (true) {
-            currentMode = currentMode.parseLine();
+            try {
+                currentMode = currentMode.parseLine();
+            }
+            catch (Exception e) { //exception <=> stdin is closed
+                break;
+            }
         }
     }
 }
 
-interface Mode {
-    Mode parseLine();
+abstract class Mode {
+    SingleTreeViewModel viewModel;
+    String prompt;
+    String helpMessage = "//TODO " + this.getClass().getSimpleName() + " help message\n";
+    abstract protected Mode _parseLine(List<String> line);
+    protected Mode parseLine() {
+        preParse();
+        List<String> line;
+        try {
+            line = CommandParser.parseStdin();
+        } catch (NoSuchElementException | IllegalStateException e) { //stdin is closed
+            throw e;
+        } catch (Exception e) {
+            System.out.println("Niepoprawna komenda:\n\t" + e.getMessage());
+            return this;
+        }
+        {
+            Mode mode = preChecks(line);
+            if(mode != null) {
+                return mode;
+            }
+        }
+        return _parseLine(line);
+    }
+    protected void preParse() {
+        System.out.print(prompt + " >> ");
+    }
+    protected Mode preChecks(List<String> line) {
+        if(line.isEmpty()) {
+            return this;
+        }
+        if(line.getFirst().equalsIgnoreCase("help")) {
+            System.out.println(helpMessage);
+            return this;
+        }
+        return null;
+    }
+    protected void commandNotFound() {
+        System.out.println("Nie znaleziono polecenia. Wpisz \"help\" aby uzyskać pomoc");
+    }
 }
 
-class BasicMode implements Mode {
+abstract class ResourceMode extends Mode {
+    Mode lastMode;
+
+    ResourceMode(SingleTreeViewModel viewModel, Mode lastMode) {
+        this.viewModel = viewModel;
+        this.lastMode = lastMode;
+    }
+
+    @Override
+    protected Mode preChecks(List<String> line) {
+        {
+            Mode mode = super.preChecks(line);
+            if(mode != null) {
+                return mode;
+            }
+        }
+        switch (line.getFirst().toLowerCase()) {
+            case "close":
+                return new BasicMode(viewModel);
+            case "back":
+                return lastMode;
+            default:
+                return null;
+        }
+    }
+}
+
+class BasicMode extends Mode {
     SingleTreeViewModel viewModel;
 
     BasicMode(SingleTreeViewModel viewModel) {
         this.viewModel = viewModel;
+        prompt = "familytree";
+        helpMessage = new HelpMessageFactory().setIntro("Dostępne komendy: ")
+                .addCommand("UPDATE", "pobiera dane osób z bazy")
+                .addCommand("SELECT PERSON id", "wybiera osobę o określonym id",
+                        List.of("id - id osoby"))
+                .addCommand("PRINTALL", "wypisuje dane wszystkich osób")
+                .generate();
     }
 
     @Override
-    public Mode parseLine() {
+    protected Mode _parseLine(List<String> line) {
         try {
-            System.out.print("basic mode >> ");
-            List<String> line = CommandParser.parseStdin();
-            if(line.isEmpty()) {
-                return this;
-            }
             switch (line.getFirst().toLowerCase()) {
-                case "help":
-                    System.out.println("TODO basicmode help message");
-                    break;
                 case "printall":
                     System.out.println("Lista osób: ");
                     for(Person person : viewModel.getGraphProperty().get().getAllPersons()) {
@@ -61,6 +129,7 @@ class BasicMode implements Mode {
                 {
                     if(line.size() < 3) {
                         System.out.println("użyj SELECT PERSON id");
+                        break;
                     }
                     switch (line.get(1).toLowerCase()) {
                         case "person":
@@ -71,7 +140,7 @@ class BasicMode implements Mode {
                                     System.out.println("Nie znaleziono osoby o id " + id);
                                     break;
                                 }
-                                return new PersonMode(viewModel, p);
+                                return new PersonMode(viewModel, this, p);
                             } catch (Exception e) {
                                 System.out.println("\"" + line.get(2) + "\" nie jest liczbą");
                                 break;
@@ -82,7 +151,7 @@ class BasicMode implements Mode {
                     break;
                 }
                 default:
-                    System.out.println("Nie znaleziono polecenia. Wpisz \"help\" aby uzyskać pomoc");
+                    commandNotFound();
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -91,32 +160,26 @@ class BasicMode implements Mode {
     }
 }
 
-class PersonMode implements Mode {
-    SingleTreeViewModel viewModel;
+class PersonMode extends ResourceMode {
     Person person;
 
-    PersonMode(SingleTreeViewModel viewModel, Person person) {
-        this.viewModel = viewModel;
+    PersonMode(SingleTreeViewModel viewModel, Mode lastMode, Person person) {
+        super(viewModel, lastMode);
         this.person = person;
+        prompt = lastMode.prompt + "/P" + person.getId();
+        helpMessage = HelpMessageFactory.resourceModeFactory()
+                .addCommand("CHANGE NAME new_name", "zmienia imię osoby",
+                        List.of("new_name - nowe imię"))
+                .generate();
     }
 
     @Override
-    public Mode parseLine() {
+    protected Mode _parseLine(List<String> line) {
         try {
-            System.out.print("person no. " + person.getId() + " >> ");
-            List<String> line = CommandParser.parseStdin();
-            if (line.isEmpty()) {
-                return this;
-            }
             switch (line.getFirst().toLowerCase()) {
-                case "help":
-                    System.out.println("TODO personmode help message");
-                    break;
                 case "print":
                     System.out.println(person.getId() + ". " + person);
                     break;
-                case "close":
-                    return new BasicMode(viewModel);
                 case "change":
                     if (line.size() < 3) {
                         System.out.println("użyj CHANGE NAME new_value");
@@ -131,11 +194,64 @@ class PersonMode implements Mode {
                     }
                     break;
                 default:
-                    System.out.println("Nie znaleziono polecenia. Wpisz \"help\" aby uzyskać pomoc");
+                    commandNotFound();
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return this;
+    }
+}
+
+/**
+ * Helper class to make help messages of various modes consistent.
+ * Pretty much self-explanatory.
+ */
+
+final class HelpMessageFactory {
+
+    static HelpMessageFactory resourceModeFactory() {
+        return new HelpMessageFactory().setIntro("Dostępne komendy:")
+                .addCommand("BACK", "wraca do poprzedniego widoku")
+                .addCommand("CLOSE", "wraca do podstawowego widoku");
+    }
+    String intro = null;
+    StringBuilder body;
+    HelpMessageFactory() {
+        body = new StringBuilder();
+        addCommand("HELP", "wyświetla tę listę");
+    }
+    public String generate() {
+        if(intro == null) {
+            throw new NullPointerException();
+        }
+        return intro + "\n" + body;
+    }
+    /**
+     * @param usage Format: `COMMAND NAME arg1 [arg3]`
+     * @param description Format: `single sentence, lower case`
+     */
+    public HelpMessageFactory addCommand(String usage, String description) {
+        return addCommand(usage, description, null);
+    }
+
+    /**
+     * @param usage Format: `COMMAND NAME arg1 [arg3]`
+     * @param description Format: `single sentence, lower case`
+     * @param args Format: `arg1 - name of the item` `arg2 (optional) - description of the item`
+     */
+    public HelpMessageFactory addCommand(String usage, String description, List<String> args) {
+        body.append(usage).append(" - ").append(description).append("\n");
+        if(args != null) {
+            for(String s : args) {
+                body.append("\t").append(s).append("\n");
+            }
+        }
+        return this;
+    }
+
+    public HelpMessageFactory setIntro(String intro) {
+        this.intro = intro;
         return this;
     }
 }
