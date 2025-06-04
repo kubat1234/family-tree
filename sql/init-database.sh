eed3f0d979b908a2d71208b20643b525399d4938
Jakub Trela <jakubtrela05@gmail.com>
2025-06-04 20:14:24 +0200

    init database script

---
+++ sql/init-database.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo -u postgres psql -d family_tree < clear.sql
+sudo -u postgres psql -d family_tree < create.sql
+sudo -u postgres psql -d family_tree -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO family_tree;"
+
