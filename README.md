979f9385b6766f99ccb75a5e9bd68ec4a23ae968
Jakub Trela <jakubtrela05@gmail.com>
2025-06-08 23:32:48 +0200

    installation

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # family-tree
 Project for OOP 2025 classes
+
+# Installation
+
+`docker compose up`
+
+`sudo docker exec -i family_tree psql -U family_tree < sql/clear.sql`
+
+`sudo docker exec -i family_tree psql -U family_tree < sql/create.sql`
+
+`mvn clean`
+
+`mvn javafx:run`
--- src/main/java/tcs/familytree/services/database/ConnectionConsts.java
+++ src/main/java/tcs/familytree/services/database/ConnectionConsts.java
@@ -3,5 +3,8 @@ package tcs.familytree.services.database;
 public class ConnectionConsts {
     final static String userName = "family_tree";
     final static String password = "family_tree";
+//    for docker database
     final static String url = "jdbc:postgresql://localhost:5433/family_tree";
+    // for local database
+//    final static String url = "jdbc:postgresql://localhost:5432/family_tree";
 }
